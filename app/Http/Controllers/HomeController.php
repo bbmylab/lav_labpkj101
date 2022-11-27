9a2b8b88e6256bccb8d7d528314d84879903ca11
WTSDevTeam <tyod@hotmail.com>
2022-11-27 20:34:44 +0700

    update 27/11/2022

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,10 +12,43 @@ class HomeController extends Controller {
 
     public function home() {
 
-        $name = 'pakkard 11111111111111111';
+        $name = 'pakkard 11111111111111111 - ';
+        //การประกาศตัวแปร
+        $sur_name = 150;
+        $name = $name . $sur_name; //การรวม string ใช้ .
+
+        //การประกาศตัวแปรแบบตัวเลช และการบวก
+        $X = 150;
+        $Y = 150;
+        $Z = $X + $Y;
+
+        //เงื่อนไข
+        if ($X > $Y) {
+            $name = 'X มากกว่า Y';
+        } else {
+            $name = 'X น้อยกว่า Y';
+        }
+
+        $val1 = 100;
+        $val7 = 999;
         
-        return view('test.home2', [
-            "name" => $name
-        ]);
+        $var = [
+            "name" => $name,
+            "val1" => $val1,
+            "val2" => $val1,
+            "val3" => $val1,
+            "val4" => $val1,
+            "val5" => $val1,
+            "val6" => $val1,
+            "val7" => $val7,
+            "val8" => $this->plus($val1,$val7),
+        ];
+
+        return view('test.home2', $var);
+
+    }
+
+    private function plus($a, $b) {
+        return $a+$b;
     }
 }
--- resources/views/test/home2.blade.php
+++ resources/views/test/home2.blade.php
@@ -3,6 +3,13 @@
 
 @section('main-content')
 
-    <h5>{{$name}}</h5>
+    <h1>{{$name}}</h1>
+    <h4>{{$val8}}</h4>
+
+    @if (20 > $val1)
+        <h1> 20 มากกว่า {{$val1}}</h1>
+    @else
+        <h1> 20 น้อยกว่า {{$val1}}</h1>
+    @endif
 
 @stop
